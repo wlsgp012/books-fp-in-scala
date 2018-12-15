6d5fed50c7b89fc7fdfcc8dedf4019b604e60160
jinhye <jinhye.kim@navercorp.com>
2018-12-15 17:12:56 +0900

    2.1

---
+++ src/main/scala/ch2/MyModule.scala
@@ -0,0 +1,18 @@
+package ch2
+
+// 이것은 주석!
+/* 이것도 주석 */
+/** 문서화 주석 */
+object MyModule {
+  def abs(n: Int): Int =
+    if (n < 0) -n
+    else n
+
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  def main(args: Array[String]): Unit =
+    println(formatAbs(-42))
+}
