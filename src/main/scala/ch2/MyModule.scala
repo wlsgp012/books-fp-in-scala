64defa7c2686f9a7858e22707a1e096fdc149815
jinhye <jinhye.kim@navercorp.com>
2018-12-15 17:22:14 +0900

    2.4.1

--- src/main/scala/ch2/MyModule.scala
+++ src/main/scala/ch2/MyModule.scala
@@ -13,6 +13,14 @@ object MyModule {
     msg.format(x, abs(x))
   }
 
+  def factorial(n: Int): Int ={
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+      if (n <=0) acc
+      else go(n-1, n*acc)
+    go(n, 1)
+  }
+
   def main(args: Array[String]): Unit =
     println(formatAbs(-42))
 }
