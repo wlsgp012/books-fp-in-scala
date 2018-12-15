bb27a5173f1d05ca30ace356472e51dcb7961e12
jinhye <jinhye.kim@navercorp.com>
2018-12-15 22:43:27 +0900

    Answer 2.3~2.4

---
+++ src/main/scala/ch2/practice/Currying.scala
@@ -0,0 +1,10 @@
+package ch2.practice
+
+//p.34,35 2.3~2.4
+object Currying extends App {
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = (a: A) => (b: B) => f(a, b)
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = (a: A, b: B) => f(a)(b)
+
+}
