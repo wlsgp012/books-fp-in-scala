57f7fd1632909bec07de85704ab4a2cbd722edb2
jinhye <jinhye.kim@navercorp.com>
2018-12-15 22:47:21 +0900

    Answer 2.6

---
+++ src/main/scala/ch2/practice/Compose.scala
@@ -0,0 +1,9 @@
+package ch2.practice
+
+/**
+  * p.35 2.6
+  */
+object Compose extends App {
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C = a => f(g(a))
+}
--- src/main/scala/ch2/practice/Currying.scala
+++ src/main/scala/ch2/practice/Currying.scala
@@ -1,6 +1,8 @@
 package ch2.practice
 
-//p.34,35 2.3~2.4
+/**
+  * p.34,35 2.3~2.4
+  */
 object Currying extends App {
 
   def curry[A, B, C](f: (A, B) => C): A => (B => C) = (a: A) => (b: B) => f(a, b)
