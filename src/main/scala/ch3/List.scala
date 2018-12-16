0e22ec00b7212e2f736de46c4014c211010f567a
jinhye <jinhye.kim@navercorp.com>
2018-12-16 21:15:01 +0900

    3.4

--- src/main/scala/ch3/List.scala
+++ src/main/scala/ch3/List.scala
@@ -25,12 +25,12 @@ object List {
   def tail[A](l: List[A]): List[A] =
     l match {
       case Nil => sys.error("tail of empty list")
-      case Cons(_,t) => t
+      case Cons(_, t) => t
     }
 
   def setHead[A](l: List[A], h: A): List[A] = l match {
     case Nil => sys.error("setHead on empty list")
-    case Cons(_,t) => Cons(h,t)
+    case Cons(_, t) => Cons(h, t)
   }
 
   def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
@@ -42,12 +42,12 @@ object List {
     if (n <= 0) l
     else l match {
       case Nil => Nil
-      case Cons(_,t) => drop(t, n-1)
+      case Cons(_, t) => drop(t, n - 1)
     }
 
   def dropWhile[A](l: List[A])(f: A => Boolean): List[A] =
     l match {
-      case Cons(h,t) if f(h) => dropWhile(t)(f)
+      case Cons(h, t) if f(h) => dropWhile(t)(f)
       case _ => l
     }
 
@@ -55,19 +55,30 @@ object List {
   def init[A](l: List[A]): List[A] =
     l match {
       case Nil => sys.error("init of empty list")
-      case Cons(_,Nil) => Nil
-      case Cons(h,t) => Cons(h,init(t))
+      case Cons(_, Nil) => Nil
+      case Cons(h, t) => Cons(h, init(t))
     }
 
   def init2[A](l: List[A]): List[A] = {
     import collection.mutable.ListBuffer
     val buf = new ListBuffer[A]
+
     @annotation.tailrec
     def go(cur: List[A]): List[A] = cur match {
       case Nil => sys.error("init of empty list")
-      case Cons(_,Nil) => List(buf.toList: _*)
-      case Cons(h,t) => buf += h; go(t)
+      case Cons(_, Nil) => List(buf.toList: _*)
+      case Cons(h, t) => buf += h; go(t)
     }
+
     go(l)
   }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+
+  def sum2(ns: List[Int]) = foldRight(ns, 0)((x, y) => x + y)
+
+  def product2(ns: List[Double]) = foldRight(ns, 1.0)(_ * _)
 }
