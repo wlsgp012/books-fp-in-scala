c6ca9500f74863dad03063a8c470cdb6a4786754
jinhye <jinhye.kim@navercorp.com>
2018-12-16 14:25:02 +0900

    Answer 3.3

--- src/main/scala/ch2/practice/Currying.scala
+++ src/main/scala/ch2/practice/Currying.scala
@@ -9,4 +9,5 @@ object Currying extends App {
 
   def uncurry[A, B, C](f: A => B => C): (A, B) => C = (a: A, b: B) => f(a)(b)
 
+  def partial[A, B, C](a: A, f:(A,B) => C): B => C = b => f(a, b)
 }
---
+++ src/main/scala/ch3/practice/SetHead.scala
@@ -0,0 +1,17 @@
+package ch3.practice
+
+import ch3.{Cons, List, Nil}
+
+/**
+  * p.45 3.3
+  */
+object SetHead extends App {
+
+  def setHead[A](list: List[A], head: A): List[A] = list match {
+    case Nil => Cons(head, Nil)
+    case Cons(_, _) => Cons(head, list)
+  }
+
+  println(setHead(List(1, 2, 3, 4, 5), 0))
+  println(setHead(Nil, 0))
+}
