83222393c2b1a1a75dfc2704d28aa0c39ec81d71
jinhye <jinhye.kim@navercorp.com>
2018-12-16 14:28:48 +0900

    Modify answer 3.3

--- src/main/scala/ch3/practice/SetHead.scala
+++ src/main/scala/ch3/practice/SetHead.scala
@@ -9,7 +9,7 @@ object SetHead extends App {
 
   def setHead[A](list: List[A], head: A): List[A] = list match {
     case Nil => Cons(head, Nil)
-    case Cons(_, _) => Cons(head, list)
+    case Cons(_, tail) => Cons(head, tail)
   }
 
   println(setHead(List(1, 2, 3, 4, 5), 0))
