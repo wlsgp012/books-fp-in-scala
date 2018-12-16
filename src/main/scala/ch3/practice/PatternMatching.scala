29d2eb88af84ae565adaeba29e0e35f55f0d31da
jinhye <jinhye.kim@navercorp.com>
2018-12-16 11:03:39 +0900

    Answer 3.1

---
+++ src/main/scala/ch3/practice/PatternMatching.scala
@@ -0,0 +1,22 @@
+package ch3.practice
+
+import ch3.Cons
+import ch3.List
+import ch3.List._
+import ch3.Nil
+
+/**
+  * p.43 3.1
+  */
+object PatternMatching extends App {
+
+  val x = List(1, 2, 3, 4, 5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + sum(t)
+    case _ => 101
+  }
+
+  println(x)
+}
