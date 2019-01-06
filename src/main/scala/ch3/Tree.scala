bab690050d846b98d5fbcbc14257643c7aede73e
jinhye <wlsgp012@naver.com>
2019-01-06 15:27:16 +0900

    Answer 3.25

--- src/main/scala/ch3/Tree.scala
+++ src/main/scala/ch3/Tree.scala
@@ -8,4 +8,9 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
+
 }
