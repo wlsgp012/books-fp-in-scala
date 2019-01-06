ab015c9684cb149e1ce938db555885df84ac3a5e
jinhye <jinhye.kim@navercorp.com>
2019-01-06 18:49:58 +0900

    Answer 4.2

--- src/main/scala/ch4/Option.scala
+++ src/main/scala/ch4/Option.scala
@@ -28,4 +28,14 @@ case class Some[+A](get: A) extends Option[A]
 
 case object None extends Option[Nothing]
 
+object Option {
+
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] = mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+
+}
+
 
