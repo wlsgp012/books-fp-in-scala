a091ea86df8e100981c0eeb5555a24f8171de3e6
jinhye <jinhye.kim@navercorp.com>
2019-01-06 18:23:42 +0900

    Answer 4.1

--- src/main/scala/ch4/Option.scala
+++ src/main/scala/ch4/Option.scala
@@ -2,15 +2,25 @@ package ch4
 
 sealed trait Option[+A] {
 
-  def map[B](f: A => B): Option[B]
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(a) => Some(f(a))
+    case None => None
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B]
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(a) => a
+    case None => default
+  }
 
-  def getOrElse[B >: A](default: => B): B
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f) getOrElse None
 
-  def orElse[B >: A](ob: => Option[B]): Option[B]
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this map (Some(_)) getOrElse ob
 
-  def filter(f: A => Boolean): Option[A]
+  def filter(f: A => Boolean): Option[A] = {
+    //    if(map(f) getOrElse false) this
+    //    else None
+    flatMap(a => if (f(a)) Some(a) else None)
+  }
 
 }
 
